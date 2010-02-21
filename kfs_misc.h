8618d1d1262e80493697ff0b8f4e12b328fea713
Hraban Luyat <hraban@0brg.net>
2010-02-21 20:48:43 +0100

    Read-only mirror FS compiles and minimal test succeeds.

---
+++ kfs_misc.h
@@ -0,0 +1,38 @@
+#ifndef _KFS_MISC_H
+#define _KFS_MISC_H
+
+#include <string.h>
+
+#define AR_SIZE(ar) (sizeof(ar) / sizeof((ar)[0]))
+
+static int
+min(int x, int y) {
+    return x > y ? y : x;
+}
+
+/**
+ * Concatenate two strings, preferably using given buffer. If the supplied
+ * buffer is not large enough a new character array is allocated which must
+ * eventually be freed. Check this by comparing the argument with the return
+ * value. If memory allocation fails, NULL is returned and errno is set.
+ */
+static char *
+kfs_bufstrcat(char *buf, const char *part1, const char *part2, size_t bufsize)
+{
+    /* Maybe the buffer is too small. */
+    if (strlen(part1) + strlen(part2) >= bufsize) {
+        buf = KFS_MALLOC(strlen(part1) + strlen(part2) + 1);
+        if (buf == NULL) {
+            KFS_ERROR("malloc: %s", strerror(errno));
+            return NULL;
+        }
+    }
+    if (buf != part1) {
+        strcpy(buf, part1); /* Careful: buffer overflow prone. */
+    }
+    strcat(buf, part2); /* Re: careful. */
+
+    return buf;
+}
+
+#endif // _KFS_MISC_H
