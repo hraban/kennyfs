717dc4d2ae9547ff02719a74f0b784f3fe97e471
Hraban Luyat <hraban@oma.lan>
2010-02-21 14:30:29 +0100

    Fixed debugging macros.

--- kfs_logging.h
+++ kfs_logging.h
@@ -2,73 +2,65 @@
 #define _KFS_LOGGING_H
 
 /**
- * Defines logging functions for the KennyFS project. Provides six logging
+ * Defines logging functions for the KennyFS project. Provides the following
  * levels:
- * - DEBUG
- * - INFO
- * - WARNING
- * - ERROR
- * - CRITICAL
- * - SILENT
+ * - KFS_LOGGING_TRACE
+ * - KFS_LOGGING_DEBUG
+ * - KFS_LOGGING_INFO
+ * - KFS_LOGGING_WARNING
+ * - KFS_LOGGING_ERROR
+ * - KFS_LOGGING_SILENT
  * Logging functions are available for the first five levels: kfs_debug(),
  * kfs_info(), etc.  The KFS_LOGGING_LEVEL macro can be used to select which
  * functions are defined. If NDEBUG is defined the default logging level is
  * WARNING, otherwise it is DEBUG.
  */
 
-#define DEBUG 0
-#define INFO 1
-#define WARNING 2
-#define ERROR 3
-#define CRITICAL 4
-#define SILENT 5
-
-#ifndef KFS_LOGGING_LEVEL
-#ifdef NDEBUG
-#define KFS_LOGGING_LEVEL WARNING
-#else
-#define KFS_LOGGING_LEVEL DEBUG
-#endif
-#elif KFS_LOGGING_LEVEL < DEBUG || DEBUG_LEVEL > SILENT
-#error KFS_LOGGING_LEVEL has an illegal value.
-#endif
-
-#define kfs_do_nothing(format, ...)
+#define kfs_do_nothing(format, ...) ((void *) (0))
 #define kfs_log(level, fmt, ...) fprintf(stderr, "%s: " fmt, \
         level, ## __VA_ARGS__)
 
-
-#if KFS_LOGGING_LEVEL == DEBUG
-#undef kfs_log
-#define kfs_log(level, fmt, ...) fprintf(stderr, "[%s] %s:%d %s(): " fmt, \
-        level, __FILE__, __LINE__, __func__, ## __VA_ARGS__)
-#define kfs_debug(...) kfs_log("debug", __VA_ARGS__)
+#if ! (defined(KFS_LOG_TRACE) \
+    || defined(KFS_LOG_DEBUG) \
+    || defined(KFS_LOG_INFO) \
+    || defined(KFS_LOG_WARNING) \
+    || defined(KFS_LOG_ERROR) \
+    || defined(KFS_LOG_SILENT))
+#ifdef NDEBUG
+#  define KFS_LOG_WARNING
 #else
+#  define KFS_LOG_DEBUG
+#endif
+#endif
+
+
+/* Start by defining all debug macros as being in use. */
+#define kfs_trace kfs_do_nothing
 #define kfs_debug kfs_do_nothing
-#endif
-
-#if KFS_LOGGING_LEVEL <= INFO
-#define kfs_info(...) kfs_log("info", __VA_ARGS__)
-#else
 #define kfs_info kfs_do_nothing
-#endif
-
-#if KFS_LOGGING_LEVEL <= WARNING
-#define kfs_warning(...) kfs_log("warning", __VA_ARGS__)
-#else
 #define kfs_warning kfs_do_nothing
-#endif
-
-#if KFS_LOGGING_LEVEL <= ERROR
-#define kfs_error(...) kfs_log("ERROR", __VA_ARGS__)
-#else
 #define kfs_error kfs_do_nothing
-#endif
 
-#if KFS_LOGGING_LEVEL <= CRITICAL
-#define kfs_critical(...) kfs_log("CRITICAL", __VA_ARGS__)
-#else
-#define kfs_critical kfs_do_nothing
+/* Now progressively strip down all unused macros. */
+#ifndef KFS_LOG_SILENT
+#  undef kfs_error
+#  define kfs_error(...) kfs_log("ERROR", __VA_ARGS__)
+#  ifndef KFS_LOG_ERROR
+#    undef kfs_warning
+#    define kfs_warning(...) kfs_log("WARNING", __VA_ARGS__)
+#    ifndef KFS_LOG_WARNING
+#      undef kfs_info
+#      define kfs_info(...) kfs_log("info", __VA_ARGS__)
+#      ifndef KFS_LOG_INFO
+#        undef kfs_debug
+#        define kfs_debug(...) kfs_log("debug", __VA_ARGS__)
+         /* More explicit logging (for all levels). */
+#        undef kfs_log
+#        define kfs_log(level, fmt, ...) fprintf(stderr, "[%s] %s:%d %s(): " \
+                fmt, level, __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#      endif
+#    endif
+#  endif
 #endif
 
 #endif /* _KFS_LOGGING_H */
