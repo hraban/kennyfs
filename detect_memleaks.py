3239600623206f4f215a4e8bb1cad9869e0b414b
Hraban Luyat <hraban@0brg.net>
2010-07-26 14:01:34 +0200

    Adding information to output of memory leak detection script: total number of bytes (allocated and leaked).

--- detect_memleaks.py
+++ detect_memleaks.py
@@ -16,7 +16,7 @@ Example:
     ...
 
 ''' % dict(name=sys.argv[0])
-RE_MALLOC = re.compile(r'kfs_[cm]alloc.*0x(\w+)')
+RE_MALLOC = re.compile(r'kfs_[cm]alloc.*\b(\d+)\b.*0x(\w+)')
 RE_FREE = re.compile(r'kfs_free.*0x(\w+)')
 RE_TIMESTAMP = re.compile(r'\d{10}\.\d{6} ')
 
@@ -24,6 +24,8 @@ def scan(f, debuglevel=0):
     stack = []
     mallocs = {}
     totalmallocs = 0
+    totalbytes = 0
+    use_trace = True
     for linenum, line in enumerate(f):
         line = line.strip()
         linenum += 1
@@ -32,7 +34,7 @@ def scan(f, debuglevel=0):
         # If timestamps are printed, ignore them.
         if RE_TIMESTAMP.match(line):
             line = line[18:]
-        if line.startswith('[kfs_trace]'):
+        if use_trace and line.startswith('[kfs_trace]'):
             stripped = line[len('[kfs_trace] '):]
             if stripped.endswith('enter'):
                 stack.append(stripped[:-len(': enter')])
@@ -42,15 +44,20 @@ def scan(f, debuglevel=0):
         elif line.startswith('[kfs_debug] kfs_memory.c:'):
             m = RE_MALLOC.search(line)
             if m is not None:
+                nbytes = int(m.group(1))
+                memaddr = int(m.group(2), 16)
                 totalmallocs += 1
-                memaddr = int(m.group(1), 16)
-                try:
-                    func = stack[-1]
-                except IndexError:
-                    sys.exit('Malloc outside function at line %d, corrupt '
-                             'stack or trace output not enabled.' % linenum)
+                totalbytes += nbytes
+                if use_trace:
+                    try:
+                        func = stack[-1]
+                    except IndexError:
+                        use_trace = False
+                        func = None
+                else:
+                    func = None
                 assert(memaddr not in mallocs)
-                mallocs[memaddr] = (linenum, func)
+                mallocs[memaddr] = (linenum, func, nbytes)
                 continue
             m = RE_FREE.search(line)
             if m is not None:
@@ -58,14 +65,22 @@ def scan(f, debuglevel=0):
                 del mallocs[memaddr]
                 continue
     if debuglevel > 0:
-        print >> sys.stderr, 'Total number of allocations: %d' % totalmallocs
+        print >> sys.stderr, 'Total: %d bytes in %d allocations.' % (totalbytes,
+                totalmallocs)
+        print
     if mallocs:
         print 'Mallocs that were never freed:'
         print
-        print 'Line number in debug output / last function entry before malloc.'
-        for info in sorted(mallocs.itervalues()):
-            print '%d\t%s' % info
-        print 'Total memory leaks: %d' % len(mallocs)
+        print ('Line number in debug output / '
+               'bytes / '
+               'last function entry before malloc.')
+        totalbytes = 0
+        for linenum, func, nbytes in sorted(mallocs.itervalues()):
+            if func is None:
+                func = '<no trace information available>'
+            print '%d\t%d\t%s' % (linenum, nbytes, func)
+            totalbytes += nbytes
+        print 'Total memory leaks: %d (%d bytes).' % (len(mallocs), totalbytes)
         sys.exit(1)
     else:
         print 'No memory leaks detected.'
